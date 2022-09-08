9b67a40400a5ee63ee7f92fce8cc6ebcae00f185
AtalaySezen <atalaysezen@yahoo.com.tr>
2022-09-08 16:02:01 +0300

    proje eklendi

---
+++ 03-Javascript ile tıklayınca görsel değişmesi/script.js
@@ -0,0 +1,11 @@
+let imageShow = document.getElementById("imageChange");
+
+
+
+function purpleChange(){
+    imageShow.src = "./assets/image/morhoodie.png";
+}
+
+function greenChange(){
+    imageShow.src = "./assets/image/yesilhoodie.png";
+}
