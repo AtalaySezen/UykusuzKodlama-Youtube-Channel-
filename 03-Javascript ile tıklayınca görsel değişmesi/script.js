0c1ca47f93f26e81ae80fb5aedfc41c3068c2ebf
AtalaySezen <atalaysezen@yahoo.com.tr>
2022-09-09 14:29:39 +0300

    update

--- 03-Javascript ile tıklayınca görsel değişmesi/script.js
+++ 03-Javascript ile tıklayınca görsel değişmesi/script.js
@@ -1,11 +1,10 @@
 let imageShow = document.getElementById("imageChange");
 
 
+function greenChange(){
+    imageShow.src = "./assets/image/yesilhoodie.png";
+}
 
 function purpleChange(){
     imageShow.src = "./assets/image/morhoodie.png";
-}
-
-function greenChange(){
-    imageShow.src = "./assets/image/yesilhoodie.png";
 }
