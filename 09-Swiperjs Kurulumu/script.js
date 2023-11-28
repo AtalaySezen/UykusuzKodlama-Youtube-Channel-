10202facfa2e34d5c5d7260132b934c4cb0c0f52
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2023-11-28 23:59:25 +0300

    feats : swiperjs projesine breakpoint ve autoplay özelliği eklendi.

--- 09-Swiperjs Kurulumu/script.js
+++ 09-Swiperjs Kurulumu/script.js
@@ -3,4 +3,22 @@ var swiper = new Swiper(".mySwiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  autoplay: {
+    delay: 3000,
+  },
+  breakpoints:{
+    500:{
+      slidesPerView:1,
+      spaceBetween:0
+    },
+    900:{
+      slidesPerView:2,
+      spaceBetween:30
+    },
+    1300:{
+      slidesPerView:3,
+      spaceBetween:30
+    }
+
+  }
 });
